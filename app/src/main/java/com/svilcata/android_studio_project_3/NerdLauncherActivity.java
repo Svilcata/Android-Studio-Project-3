db51bec3bfa6335290eaf36fd9a082fbfaf5ac06
Svilcata <lord_solen@abv.bg>
2016-04-30 02:01:22 +0300

    Initial commit

---
+++ app/src/main/java/com/svilcata/android_studio_project_3/NerdLauncherActivity.java
@@ -0,0 +1,8 @@
+package com.svilcata.android_studio_project_3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NerdLauncherActivity extends AppCompatActivity {
+
+}
